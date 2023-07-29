34623a38ce7e020b5bb51511c5d3239ba2c7eab2
Brian Guterl <brianguterl99@gmail.com>
2023-07-29 00:55:04 -0400

    added in routes and show hide add form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { UiService } from '../../services/ui.service';
 
 import { Task } from '../../Task';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -12,6 +14,14 @@ export class AddTaskComponent {
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask!: boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   onSubmit() {
     if (!this.text) {
